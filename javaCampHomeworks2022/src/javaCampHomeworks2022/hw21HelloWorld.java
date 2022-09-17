f4ee6b8358f1884b4005714b098def676dc0b843
iamnothing <iamnothing@iamnothin>
2022-09-17 16:08:53 +0300

    javaCampHomeworks2022 - Includes all course homeworks.

---
+++ javaCampHomeworks2022/src/javaCampHomeworks2022/hw21HelloWorld.java
@@ -0,0 +1,11 @@
+package javaCampHomeworks2022;
+
+public class hw21HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ javaCampHomeworks2022/src/javaCampHomeworks2022/hw22Variables.java
@@ -0,0 +1,14 @@
+package javaCampHomeworks2022;
+
+public class hw22Variables {
+
+	// reusability
+
+	public static void main(String[] args) {
+		int numberOfStudent = 10;
+		String message = "Number of Student : ";
+		System.out.println(message + numberOfStudent);
+
+	}
+
+}
