0b88082ebcd07c50e700df5ee2db77810c6fc963
iamnothing <iamnothing@iamnothin>
2022-09-17 12:26:23 +0300

    Homework2

---
+++ dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,52 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// boolean 1 byte
+
+		// char 2 bytes
+
+		// byte 1 byte
+		// short 2 byte
+		// int 4 bytes
+		// long 8 bytes
+
+		// float 4 bytes
+		// double 8 bytes
+
+		byte number1 = 127;
+		System.out.println(number1);
+		byte number2 = -128;
+		System.out.println(number2);
+
+		short number3 = 32767;
+		System.out.println(number3);
+		short number4 = -32768;
+		System.out.println(number4);
+
+		int number5 = 2147483647;
+		System.out.println(number5);
+
+		// TC ID number is defined as long.
+		// But as best practice it should be "String".
+
+		double number6 = 12.5;
+		System.out.println(number6);
+
+		// Should be ''
+		// ASCII
+		char character = 'A';
+		System.out.println(character);
+
+		String city = "İstanbul";
+		System.out.println(city);
+
+		boolean isThatTrue = true;
+		boolean isThatTruee = false;
+
+		// For the more information about datatypes use images folder.
+
+	}
+
+}
