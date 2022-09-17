91fc64878b20e34eefe260a968e73b23e600f46f
iamnothing <iamnothing@iamnothin>
2022-09-17 13:03:30 +0300

    Homework2

--- recapDemo1/src/recapDemo1/Main.java
+++ recapDemo1/src/recapDemo1/Main.java
@@ -4,9 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		int number1 = 20;
+		int number1 = 30;
 		int number2 = 25;
 		int number3 = 2;
+		int theBiggest = number1;
+		
+		if(theBiggest<number2) {
+			theBiggest = number2;
+		}
+		
+		if(theBiggest<number3) {
+			theBiggest = number3;
+		}
+		
+		System.out.println("theBiggest = "+theBiggest);
 
-	}
+	} 
 }
