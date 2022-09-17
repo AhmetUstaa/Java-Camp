6d3ec447113e2457a73facafd521cf17d5eac1c8
iamnothing <iamnothing@iamnothin>
2022-09-17 12:25:21 +0300

    Homework2

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,14 @@
+package variables;
+
+public class Main {
+
+//reusability
+
+	public static void main(String[] args) {
+		int numberOfStudent = 10;
+		String message = "Number of Student : ";
+		System.out.println(message + numberOfStudent);
+
+	}
+
+}
