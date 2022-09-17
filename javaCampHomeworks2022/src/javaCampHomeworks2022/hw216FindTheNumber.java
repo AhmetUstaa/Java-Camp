7221ceff94fce35ff5096d3940213cd2c63815c0
iamnothing <iamnothing@iamnothin>
2022-09-17 22:50:22 +0300

    javaCampHomeworks2022 - Includes all course homeworks.

---
+++ javaCampHomeworks2022/src/javaCampHomeworks2022/hw216FindTheNumber.java
@@ -0,0 +1,23 @@
+package javaCampHomeworks2022;
+
+public class hw216FindTheNumber {
+
+	public static void main(String[] args) {
+		int[] numbers = new int[] { 1, 2, 5, 7, 9, 0 };
+		int findThisNumber = 5;
+		boolean anyHave = false;
+
+		for (int number : numbers) {
+			if (number == findThisNumber) {
+				anyHave = true;
+				break;
+			}
+		}
+		if (findThisNumber) {
+			System.out.println("I am hereeeee!!!!");
+		} else {
+			System.out.println("Nope, try again loooooser :)");
+		}
+	}
+
+}
