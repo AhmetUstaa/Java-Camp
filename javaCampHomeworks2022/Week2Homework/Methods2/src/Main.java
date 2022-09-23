aebff7c781b02d4df9cd1873123add6e533853fc
Ahmet Usta <108552711+AhmetUstaa@users.noreply.github.com>
2022-09-23 12:53:10 +0300

    Week2Homework2

---
+++ javaCampHomeworks2022/Week2Homework/Methods2/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String message = "Today is a good day to love.";
+		String newMessage = message.substring(0, 2);
+		System.out.println(newMessage);
+	}
+
+	public static void add() {
+		System.out.println("Added");
+	}
+
+	public static void delete() {
+		System.out.println("Deleted");
+	}
+
+	public static void update() {
+		System.out.println("Updated");
+	}
+
+}
