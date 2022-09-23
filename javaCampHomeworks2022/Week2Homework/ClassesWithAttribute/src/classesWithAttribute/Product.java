ca7d2051e8cd1c07d9975c7cf022edb7fa2c51bc
Ahmet Usta <108552711+AhmetUstaa@users.noreply.github.com>
2022-09-23 20:30:33 +0300

    Week2Homework2

---
+++ javaCampHomeworks2022/Week2Homework/ClassesWithAttribute/src/classesWithAttribute/Product.java
@@ -0,0 +1,12 @@
+package classesWithAttribute;
+
+public class Product {
+	
+	// attribute | field
+	int id;
+	String name;
+	String description;
+	double price;
+	int stockAmount;
+
+}
---
+++ javaCampHomeworks2022/Week2Homework/ClassesWithAttribute/src/classesWithAttribute/ProductManager.java
@@ -0,0 +1,9 @@
+package classesWithAttribute;
+
+public class ProductManager {
+	public void Add(Product product) {
+		// JDBC
+		System.out.println("The product added " + product.name);
+	}
+
+}
