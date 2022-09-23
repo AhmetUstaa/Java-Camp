94b6e950722fb76bce599879778d9ac358bad6a6
Ahmet Usta <108552711+AhmetUstaa@users.noreply.github.com>
2022-09-23 22:36:06 +0300

    Week2Homework2

---
+++ javaCampHomeworks2022/Week2Homework/Inheritance/src/inheritance/EmployeeManager.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class EmployeeManager extends PersonManager {
+
+	public void BestEmployee() {
+		System.out.println("The employee of the month... ");
+
+	}
+
+}
---
+++ javaCampHomeworks2022/Week2Homework/Inheritance/src/inheritance/PersonManager.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class PersonManager {
+	public void List() {
+		System.out.println("The client is listed.");
+
+	}
+
+	public void Add() {
+		System.out.println("The client is added.");
+
+	}
+
+}
