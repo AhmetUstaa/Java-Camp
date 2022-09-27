dde4cb382477208af6cd7cd3b27df8d8a717f8c6
Ahmet Usta <108552711+AhmetUstaa@users.noreply.github.com>
2022-09-27 11:52:08 +0300

    Week2Homework2
    Extra Homework

--- javaCampHomeworks2022/Week2Homework/Interfaces/src/interfaces/Main.java
+++ javaCampHomeworks2022/Week2Homework/Interfaces/src/interfaces/Main.java
@@ -3,8 +3,9 @@ package interfaces;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		CustomerManager customerManager = new CustomerManager(new OracleCustomerDal());
 
+		customerManager.add();
 	}
 
 }
